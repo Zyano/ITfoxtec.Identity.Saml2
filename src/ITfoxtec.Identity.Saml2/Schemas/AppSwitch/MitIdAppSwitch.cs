e50de1b174510106a6556abdd679e4843b6595c6
Stefan Weibel <stefan.weibel@gmail.com>
2023-10-27 12:35:45 +0200

    Add MitId app switch

---
+++ src/ITfoxtec.Identity.Saml2/Schemas/AppSwitch/MitIdAppSwitch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ITfoxtec.Identity.Saml2.Schemas.AppSwitch
+{
+    public class MitIdAppSwitch
+    {
+        const string elementName = MitIdAppSwitchConstants.ElementName;
+
+        public string Platform { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public XElement ToXElement()
+        {
+            var envelope = new XElement(MitIdAppSwitchConstants.XNamespace + elementName);
+
+            envelope.Add(GetXContent());
+
+            return envelope;
+        }
+
+        protected virtual IEnumerable<XObject> GetXContent()
+        {
+            if (Platform != null)
+            {
+                yield return new XElement(MitIdAppSwitchConstants.Platform, Platform);
+            }
+
+            if (ReturnUrl != null)
+            {
+                yield return new XElement(MitIdAppSwitchConstants.ReturnUrl, ReturnUrl);
+            }
+        }
+    }
+}
---
+++ src/ITfoxtec.Identity.Saml2/Schemas/AppSwitch/MitIdAppSwitchConstants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ITfoxtec.Identity.Saml2.Schemas.AppSwitch
+{
+    public class MitIdAppSwitchConstants
+    {
+        public static readonly string Namespace = "https://data.gov.dk/eid/saml/extensions";
+
+        public static readonly XNamespace XNamespace = XNamespace.Get(Namespace);
+
+        public const string ElementName = "AppSwitch";
+
+        public const string Platform = "Platform";
+
+        public const string ReturnUrl = "ReturnUrl";
+    }
+}
